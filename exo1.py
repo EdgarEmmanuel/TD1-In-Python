60f5f085ae926f5777422f05dd32230b57a3c319
Edgar <mmnledgar@gmail.com>
2020-05-19 09:29:38 +0000

    Creation fichier et redaction du code pour exercice1

---
+++ exo1.py
@@ -0,0 +1,10 @@
+
+import math
+
+
+a = int(input("saisissez le nombre a : "))
+b = int(input("saisissez le nombre b : "))
+
+print("le quotient entier est : ",math.floor(a/b))
+print("le reste de la division  est : ",a%b)
+print("le quotient reel est : ",a/b)
