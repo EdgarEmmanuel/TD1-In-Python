5aa39048be71f1bab2b5897543721d904a91cc10
Edgar <mmnledgar@gmail.com>
2020-05-21 17:41:48 +0000

    modification algorithme exercice 22

--- exo22.py
+++ exo22.py
@@ -1,12 +1,28 @@
 #in python
 
+import sys
 
-array = [1,2,3,1,2,3,4,5,6,7,8,2,3,4,5]
+array=[]
+
+n = int(input("Combien de chiffres voulez vous dans le tableau : "))
+
+for k in range(0,n):
+    val = int(input("saisir la valeur du nombre : "))
+    array.append(val)
+
+print("----------- Affichage du Tableau ------------------")
+
+for k in range(0,n):
+    sys.stdout.write(str(array[k])+" | ")
+
+print(" ")#pour aller a la ligne suivante 
+#array = [1,2,3,1,2,3,4,5,6,7,8,9,2,3,4,5]
 
 svg = 0 
 cpt=0
+length = len(array)
 for i in range (0,len(array)):
-    if((array[i+1] is not None)):
+    if(i!=length-1):
         if(array[i]<array[i+1]):
             cpt+=1
             last=array[i+1]
@@ -21,11 +37,15 @@ for i in range (0,len(array)):
             last=0
 
 print("la longeur de la suite est : ",svg)
-print("le dernier nombr de la suite est",svg2," , il ets a la position : ",posi)
+print("le dernier nombre de la suite est",svg2)
+print("il est a la position : ",posi," du tableau !!")
+
 print("--------- Tableau de la suite Croissante----------")
 
-for j in range((posi-svg),posi):
-    print(array[j])
+for j in range((posi-svg),posi+1):
+    sys.stdout.write(str(array[j])+" | ")
+
+print(" ")
 
 
 
